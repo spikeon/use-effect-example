878f9d9e2779a1f74488ae4bbaf954874cc8f526
Mike Flynn <spikeon@gmail.com>
2021-01-30 22:16:54 -0800

    Using custom hooks

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import React from 'react';
 import UseEffectExample from './components/use-effect-example/use-effect-example.component';
 
 import './App.css';
+import User from './components/user/user.component';
+import Post from './components/post/post.component';
 
 const App = props => {
-  return <div className='App'><UseEffectExample /></div>;
+  return (
+      <div className='App'>
+        <UseEffectExample />
+        <User userId={5} />
+        <Post postId={15} />
+      </div>
+  );
 };
 
 export default App;
--- src/components/user/user.component.jsx
+++ src/components/user/user.component.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 
 import Card from '../card/card.component';
-
 import useFetch from '../../effects/use-fetch.effect';
 
-const User = ({ userId }) => {
-  const user = useFetch(
-    `https://jsonplaceholder.typicode.com/users?id=${userId}`
-  );
+const User = ({userId}) => {
 
-  return (
-    <Card>
-      {user ? (
-        <div>
-          <h3>{user.username}</h3>
-          <p>{user.name}</p>
-        </div>
-      ) : (
-        <p>User not found</p>
-      )}
-    </Card>
-  );
+    const user = useFetch(
+        `https://jsonplaceholder.typicode.com/users?id=${userId}`
+    );
+
+    return (
+        <Card>
+            {user ? (
+                <div>
+                    <h3>{user.username}</h3>
+                    <p>{user.name}</p>
+                </div>
+            ) : (
+                <p>User not found</p>
+            )}
+        </Card>
+    );
 };
 
 export default User;
