39972c0b254cf411c4674f8dbafdbd65afa63452
Mike Flynn <spikeon@gmail.com>
2021-01-30 22:34:51 -0800

    Using useReducer

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import UseEffectExample from './components/use-effect-example/use-effect-example
 import './App.css';
 import User from './components/user/user.component';
 import Post from './components/post/post.component';
+import UseReducerExample from './components/use-reducer-example/use-reducer-example.component';
 
 const App = props => {
-  return (
-      <div className='App'>
-        <UseEffectExample />
-        <User userId={5} />
-        <Post postId={15} />
-      </div>
-  );
+    return (
+        <div className='App'>
+            <UseReducerExample />
+            <UseEffectExample />
+            <User userId={5} />
+            <Post postId={15} />
+        </div>
+    );
 };
 
 export default App;
